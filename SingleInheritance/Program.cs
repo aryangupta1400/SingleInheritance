f849932c54cbcb60fa9d14ab97d035d0665ea123
Aryan.Gupta <Aryan.Gupta@BRD-3917L13-L>
2022-10-29 15:53:12 +0530

    Add project files.

---
+++ SingleInheritance/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// 7) Create one example for single inheritance
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
