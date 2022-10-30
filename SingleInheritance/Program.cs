8fe04cc9a2a17b08dc8b38ce4684441feaaa2d79
Aryan Gupta <Aryan.Gupta@oneadvanced.com>
2022-10-30 18:50:39 +0530

    code modified

--- SingleInheritance/Program.cs
+++ SingleInheritance/Program.cs
@@ -13,6 +13,43 @@ namespace SingleInheritance
     {
         static void Main(string[] args)
         {
+            Apple apple = new Apple();
+            apple.Display();
+
+            Mango mango = new Mango();  
+            mango.Display();
+
+            Console.ReadKey();
         }
     }
+
+    class Fruit
+    {
+        public void FruitColor(string fruitName, string color)
+        {
+            Console.WriteLine("The color of " + fruitName + " is " + color);
+        }
+    }
+
+    class Apple : Fruit
+    {
+        public void Display()
+        {
+            Fruit fruit = new Fruit();
+            fruit.FruitColor("Apple", "Red");
+            
+        }
+    }
+
+    class Mango : Fruit
+    {
+        public void Display()
+        {
+            Fruit fruit = new Fruit();
+            fruit.FruitColor("Mango", "Yellow");
+
+        }
+    }
+
+    
 }
